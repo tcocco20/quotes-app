0879f420e0ec240bb1c1001e3a00c0045ad937c3
Theo Rafael Cocco <theococco@yahoo.com>
2022-09-28 15:03:17 -0700

    added 404 page and quote details

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,13 +2,25 @@ import React from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+  ]
+
 function QuoteDetail() {
   const params = useParams();
+  
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
 
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
