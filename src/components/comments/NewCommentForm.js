0327cdb212b8f302f8d375720f1e553d8e2e5dc1
tcocco20 <theococco@yahoo.com>
2022-10-01 15:46:55 -0700

    started comments functionality

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,20 +1,36 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = props => {
+  const { sendRequest, status } = useHttp(addComment);
   const commentTextRef = useRef();
+
+  // useEffect(() => {
+  //   if (status ) {}
+  // }, [status, error])
 
   const submitFormHandler = event => {
     event.preventDefault();
 
+    const enteredText = commentTextRef.current.value;
+
     // optional: Could validate here
 
     // send comment to server
+    sendRequest({ text: enteredText });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor="comment">Your Comment</label>
         <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
